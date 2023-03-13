62ea91afe21230e37540e668d053667dcb504fa6
Nestori <nessu.kangashaka@gmail.com>
2023-03-13 20:58:33 +0200

    Started the dashboard from the ground and the search bar and filter is done with emails

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -34,10 +34,35 @@ function SearchResult(props)
     )
 };
 
+function ResultList(props)
+{
+    const emails = props.emails.filter(email => email.email.includes(props.searchTerm));
+    const items = emails.slice(0, 5);
+    return (
+        <div className='SearchListBackground'>
+            <ul style={{padding: "0", margin: "5px 0 5px 0"}}>
+                {items.length !== 0 ? ( <div>
+                    {items.map((email, index) => 
+                    (
+                        <li key={index} className="SearchResultItem">{email.email}</li>
+                    ))} </div> )
+                    : 
+                    ( <div>
+                        <p className='EmptySearchBar' >
+                            No results
+                        </p>
+                    </div> )
+                }
+            </ul>
+        </div>
+    )
+}
+
 function AdminDashboard(props)
 {
     const ArtistRef = useRef(null);
     const ExhibitionRef = useRef(null);
+    const InformationRef = useRef(null);
 
     const navigate = useNavigate();
     const [adminIDOne, setAdminIDOne] = useState("");
@@ -45,6 +70,8 @@ function AdminDashboard(props)
     const [passwordOne, setPasswordOne] = useState("");
     const [passwordTwo, setPasswordTwo] = useState("");
     const [emails, setEmails] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchSelect, setSearchSelect] = useState("");
     
     const showToast = (message, type) =>
     {
@@ -77,10 +104,6 @@ function AdminDashboard(props)
             );
         }
     }
-    const handleIDOneChange = (event) => { setAdminIDOne(event.target.value); }
-    const handleIDTwoChange = (event) => { setAdminIDTwo(event.target.value); }
-    const handlePasswordOneChange = (event) => { setPasswordOne(event.target.value); }
-    const handlePasswordTwoChange = (event) => { setPasswordTwo(event.target.value); }
     const updateAdminID = () =>
     {
         if (adminIDOne === adminIDTwo)
@@ -204,107 +227,34 @@ function AdminDashboard(props)
     return (
         <div className='DashboardBackground'>
 
-            <div className='DashboardTop'>
-                <h1 className='DashboardHeader' style={{fontSize: "xx-large"}} >ADMIN DASHBOARD</h1>
+            <div className='Navbar'>
+                <p className='NavbarLink'>ARTISTS</p>
+                <p className='NavbarLink'>EXHIBITIONS</p>
+                <p className='NavbarLink'>INFORMATION</p>
+                <p className='NavbarLink' onClick={logout} >LOGOUT</p>
             </div>
 
-            <div className='HorizontalSeparator' />
-
-            <div className='DashboardMiddle'>
-
-                <div className='DashboardMiddleSide'>
-
-                    <div className='EmailList'>
-                        <div style={{flexDirection: "row", display: "flex", justifyContent: "space-between"}}>
-                            <h3 className='DashboardHeader'>SUBSCRIBTION EMAIL LIST</h3>
-                            <h3 className='DashboardHeader'>TOTAL : {emails.length}</h3>
-                        </div>
-                        <div className='HorizontalSeparator' />
-                        {emails.length === 0 ? (
-                            <div>
-                                <p className='Email' style={{fontStyle: "italic", fontWeight: "200"}} >No subscribtions yet</p>
-                            </div> )
-                        :
-                        ( <div>
-                                {emails.map((object, index) => (
-                                    <p key={index} className="Email">{object.email}</p>
-                                ))}
-                            </div> )}
-                    </div>
-                </div>
-
-                <div className='DashboardArtExhContainer'>
-                    
-                    <div className='DashboardExhibitionsBackground' ref={ExhibitionRef}>
-                        
-                        <div className='DashboardExhibitionsArea'>
-                            
-                        </div>
-
-                        <div style={{width: "100%", flex: 1, display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
-                            <h1 className='DashboardLink' style={{margin: "0"}} onClick={() => scrollToView(ArtistRef)}>
-                                ARTISTS
-                            </h1>
-                            <IoIosArrowDown className='LinkArrow' />
-                        </div>
-
-                    </div>
-
-                    <div className='DashboardArtistsBackground' ref={ArtistRef}>
-
-                        <div style={{width: "100%", flex: 1, display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
-                            <IoIosArrowUp className='LinkArrow' />
-                            <h1 className='DashboardLink' style={{margin: "0 0 10px 0"}} onClick={() => scrollToView(ExhibitionRef)}>
-                                EXHIBITIONS
-                            </h1>
-                        </div>
-    
-                        <div className='DashboardArtistsArea'>
-                            <h1 className='DashboardHeader'>SEARCH ARTIST</h1>
-                            <div className='SearchBarArea'>
-                                <input type="text" placeholder='Search' className='Input' style={{width: "100%"}} />
-                                <input type="button" value="Search" className='Button' />
-                            </div>
-
-                            <div className='HorizontalSeparator' style={{width: "80%"}} />
-                            
-                            <SearchResult />
-                            <div className='HorizontalSeparator' style={{width: "80%"}} />
-
-                            <div className='NewArtistArea'>
-                                    <h1 className='DashboardHeader'>ADD NEW ARTIST</h1>
-                                <div className='ResultInputGroup' >
-                                    <input type="text" className='Input' placeholder="Artists' name" />
-                                    <textarea type="text" className='Textarea' placeholder="Bio" rows={4} />
-                                    <input type="text" className='Input' placeholder="Path to the image" />
-                                </div> 
-                                <div>
-                                    <input type="button" className='Button' value="Add" />
-                                </div>
-                            </div>
-                        </div>
-                
-                    </div>
-
-                </div>
-
-                <div className='DashboardMiddleSide'>
-                    <h3 className='DashboardHeader'>UPDATE LOGIN INFORMATION</h3>
-
-                    <input type="text" placeholder='New ID' value={adminIDOne} onChange={handleIDOneChange} className="Input" />
-                    <input type="text" placeholder='Confirm ID' value={adminIDTwo} onChange={handleIDTwoChange} className="Input" />
-                    <input type="button" value="Update" className='Button' onClick={updateAdminID} />
-
-                    <input type="password" placeholder='New Password' value={passwordOne} onChange={handlePasswordOneChange} className="Input" />
-                    <input type="password" placeholder='Confirm Password' value={passwordTwo} onChange={handlePasswordTwoChange} className="Input" />
-                    <input type="button" value="Update" className='Button' onClick={updatePassword} />
-                </div>
+            <div className='SearchBarArea'>
+                <input  type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="Input" placeholder='Search artists or exhibitions' />
+                {searchTerm !== "" &&
+                    <ResultList emails={emails} searchTerm={searchTerm} />
+                }
             </div>
 
-            <div className='HorizontalSeparator' />
+            <div className='SearchResultArea'>
 
-            <div className='DashboardBottom'>
-                <p className='DashboardLink' onClick={logout}>LOGOUT</p>
+            </div>
+
+            <div className='NewArtistsArea'>
+
+            </div>
+
+            <div className='NewExhibitionsArea'>
+
+            </div>
+
+            <div className='InformationArea'>
+
             </div>
 
             <ToastContainer />
