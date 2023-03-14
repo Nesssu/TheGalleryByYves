a38dab687ed1cdc39214b75fd5fa4386745af390
Nestori <nessu.kangashaka@gmail.com>
2023-03-14 15:18:18 +0200

    Artist/Exhibition Add/Edit/Delete/Search template ready

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -3,12 +3,45 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
-function SearchResultArtist(props)
+function DataTempalte(props)
 {
-    const [name, setName] = useState(props.name);
-    const [bio, setBio] = useState(props.bio);
+    const [searchData, setSearchData] = useState(props.searchData ? props.searchData : {});
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
     const [path, setPath] = useState(null);
+    const fileInputRef = useRef(null);
+    const handleClick = () => { fileInputRef.current.click() }
 
+    const handleNameChange = (event) => { setSearchData({...searchData, name: event.target.value}) }
+    const handleBioChange = (event) => { setSearchData({...searchData, bio: event.target.value}) }
+    const handleTitleChange = (event) => { setSearchData({...searchData, title: event.target.value}) }
+    const handleArtistChange = (event) => { setSearchData({...searchData, artist: event.target.value}) }
+    const handleAboutChange = (event) => { setSearchData({...searchData, about: event.target.value}) }
+
+    const handleArtisDelete = () =>
+    {
+
+    }
+    const handleArtistSave = () =>
+    {
+
+    }
+    const handleArtistAdd = () =>
+    {
+
+    }
+    const handleExhibitionDelete = () =>
+    {
+
+    }
+    const handleExhibitionSave = () =>
+    {
+
+    }
+    const handleExhibitionAdd = () =>
+    {
+
+    }
     const handlePathChange = (event) =>
     {
         const file = event.target.files[0];
@@ -18,21 +51,55 @@ function SearchResultArtist(props)
         {
             setPath(e.target.result);
         }
-
         reader.readAsDataURL(file);
     }
+
+    useEffect(() => 
+    {  
+        console.log(searchData.name);
+    }, [props]);
 
     return (
         <div className='SearchResultArea'>
             <div className='SearchResultLeftSide'>
-                <img src={path ? path : require("../media/art_one.jpg")} className="SearchImage" alt="" />
-                <div className='ChangeImageContainer'>
-                    <input type="file" onChange={handlePathChange} style={{display: "none"}} />
+                <img src={path ? path : require("../media/no_image.jpg")} className="SearchImage" alt="" />
+                <div className='ChangeImageContainer' onClick={handleClick} >
+                    <label htmlFor='BrowseImages'>Change Image</label>
+                    <input type="file" style={{display: "none"}} onChange={handlePathChange} id="BrowseImages" ref={fileInputRef} />
+                </div>
+                {props.type === "exhibition" && (
+                    <div style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <input type={"date"} value={date} onChange={(e) => setDate(e.target.value)} className="TimeInput" style={{margin: "0 0 5px 0", width: "210px", textAlign: "center"}} />
+                        <input type={"time"} value={time} onChange={(e) => setTime(e.target.value)} className="TimeInput" style={{margin: "5px 0 5px 0", width: "210px", textAlign: "center"}} />
+                    </div>
+                )}
+                <div style={{display: "flex", alignItems: "center", width: "240px"}} >
+                    {props.protocol !== "add" ? (
+                        <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%"}} >
+                            <input type="button" className='Button' value="Delete" onClick={props.type === "exhibition" ? handleExhibitionDelete : handleArtisDelete} />
+                            <input type="button" className='Button' value="Save" onClick={props.type === "exhibition" ? handleExhibitionSave : handleArtistSave} />
+                        </div>
+                    ) : (
+                        <div>
+                            <input type="button" className='Button' style={{width: "240px"}} value="Add" onClick={props.type === "exhibition" ? handleExhibitionAdd : handleArtistAdd} />
+                        </div>
+                    )}
                 </div>
             </div>
 
-            <div className='SearchResultRightSide'>
-
+            <div style={{width: "500px", padding: "5px"}} >
+                {props.type === "artist" ? (
+                    <div className='SearchResultRightSide'>
+                        <input type="text" className='Input' value={searchData.name} onChange={handleNameChange} placeholder="Name" />
+                        <textarea className='Textarea' value={searchData.bio} onChange={handleBioChange} placeholder="Bio" />
+                    </div>
+                ) : (
+                    <div className='SearchResultRightSide'>
+                        <input type="text" className='Input' value={searchData.title} onChange={handleTitleChange} placeholder="Title" />
+                        <input type="text" className='Input' value={searchData.artist} onChange={handleArtistChange} placeholder="Artist" style={{margin: "10px 10px 0 10px"}} />
+                        <textarea className='Textarea' value={searchData.about} onChange={handleAboutChange} placeholder="About" />
+                    </div>
+                )}
             </div>
         </div>
     )
@@ -48,13 +115,13 @@ function ResultList(props)
                 {items.length !== 0 ? ( <div>
                     {items.map((email, index) => 
                     (
-                        <div className='FilterArea' onClick={() => props.search(email.email)} >
-                            <li key={index} className="SearchResultItem">{email.email}</li>
-                            <li key={index + "" + index} className="SearchResultType">Email</li>
+                        <div key={index} className='FilterArea' onClick={() => props.search(email.email)} >
+                            <li className="SearchResultItem">{email.email}</li>
+                            <li className="SearchResultType">Email</li>
                         </div>
                     ))} </div> )
                     : 
-                    ( <div>
+                    ( <div key={"abc"}>
                         <p className='EmptySearchBar' >
                             No results
                         </p>
@@ -70,6 +137,7 @@ function AdminDashboard(props)
     const ArtistRef = useRef(null);
     const ExhibitionRef = useRef(null);
     const InformationRef = useRef(null);
+    const SubscribtionRef = useRef(null);
 
     const navigate = useNavigate();
     const [adminIDOne, setAdminIDOne] = useState("");
@@ -78,7 +146,10 @@ function AdminDashboard(props)
     const [passwordTwo, setPasswordTwo] = useState("");
     const [emails, setEmails] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [searchSelect, setSearchSelect] = useState("");
+    const [showSearch, setShowSearch] = useState(false);
+    const [searchData, setSearchData] = useState({});
+    const [protocol, setProtocol] = useState("");
+    const [type, setType] = useState("");
     
     const showToast = (message, type) =>
     {
@@ -188,6 +259,10 @@ function AdminDashboard(props)
     const scrollToView = (ref) => { ref.current.scrollIntoView(); }
     const search = (value) =>
     {
+        setSearchData({ ...searchData, name: value, bio: value + "s' bio will be here!" })
+        setType("artist");
+        setProtocol("edit")
+        setShowSearch(true);
         setSearchTerm("");
     }
 
@@ -233,15 +308,16 @@ function AdminDashboard(props)
         {
             logout();
         }
-    }, []);
+    }, [searchData]);
 
     return (
         <div className='DashboardBackground'>
 
             <div className='Navbar'>
-                <p className='NavbarLink'>ARTISTS</p>
-                <p className='NavbarLink'>EXHIBITIONS</p>
-                <p className='NavbarLink'>INFORMATION</p>
+                <p className='NavbarLink' onClick={() => scrollToView(ArtistRef)}>ARTISTS</p>
+                <p className='NavbarLink' onClick={() => scrollToView(ExhibitionRef)}>EXHIBITIONS</p>
+                <p className='NavbarLink' onClick={() => scrollToView(SubscribtionRef)}>SUBSCRIBTIONS</p>
+                <p className='NavbarLink' onClick={() => scrollToView(InformationRef)}>INFORMATION</p>
                 <p className='NavbarLink' onClick={logout} >LOGOUT</p>
             </div>
 
@@ -252,17 +328,28 @@ function AdminDashboard(props)
                 }
             </div>
 
-            <SearchResultArtist />
+            {showSearch && (
+                <div style={{textAlign: "center"}}>
+                    <h1 className='DashboardHeader'>SEARCH RESULT</h1>
+                    <DataTempalte type={type} protocol={protocol} searchData={searchData} />
+                </div>
+            )}
 
-            <div className='NewArtistsArea'>
+            <div className='NewArtistArea' ref={ArtistRef}>
+                <h1 className='DashboardHeader'>ADD NEW ARTIST</h1>
+                <DataTempalte type={"artist"} protocol={"add"} />
+            </div>
+
+            <div className='NewExhibitionArea' ref={ExhibitionRef}>
+                <h1 className='DashboardHeader'>ADD NEW EXHIBITION</h1>
+                <DataTempalte type={"exhibition"} protocol={"add"} />
+            </div>
+
+            <div className='SubscribtionArea' ref={SubscribtionRef}>
 
             </div>
 
-            <div className='NewExhibitionsArea'>
-
-            </div>
-
-            <div className='InformationArea'>
+            <div className='InformationArea' ref={InformationRef}>
 
             </div>
 
